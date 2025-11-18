5af93a490cc04035e23258cd2a05726b071bea9c
Wili <wi.caceres@duocuc.cl>
2025-11-18 11:21:52 -0300

    commit de prueba

--- Levelup/app/src/main/java/com/example/levelup/MainActivity.kt
+++ Levelup/app/src/main/java/com/example/levelup/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import com.example.levelup.navigation.AppNavigation
 import com.example.levelup.ui.theme.LevelUpTheme
 
+//aqui empieza
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
